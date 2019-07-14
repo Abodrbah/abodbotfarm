d201e7e5ef3b197431ee6e1c55398ab807a3d2c1
Abodrbah <52882558+Abodrbah@users.noreply.github.com>
2019-07-14 14:29:05 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("599335574532259850")
+setInterval(function() {
+channel.send(`Abod Abod Abod`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
