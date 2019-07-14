63f12fb31d7940a4819c61bf907da0ef45ec0be4
Abodrbah <52882558+Abodrbah@users.noreply.github.com>
2019-07-14 14:54:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on("ready", () => {
 let channel =     client.channels.get("599335574532259850")
 setInterval(function() {
 channel.send(`Abod Abod Abod`);
-}, 20)
+}, 40)
 })
 
 client.login(process.env.BOT_TOKEN);
